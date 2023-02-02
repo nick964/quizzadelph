4b6bbf5b39176be3809c0f7d50983778c422dba2
Nick Robinson <nickr964@gmail.com>
2023-02-01 21:45:36 -0500

    Adding chakra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {   Box,
+  Center,
+  Heading,
+  Text,
+  Stack,
+  Avatar,
+  useColorModeValue } from '@chakra-ui/react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [bars, setBars] = useState([]);
+  useEffect(() => {
+      fetch('http://localhost:8080/bars/all')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            setBars(data);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ChakraProvider>
+
+      <div className="App">
+      <Center py={6}>
+      <Box
+        maxW={'445px'}
+        w={'full'}
+        bg={useColorModeValue('white', 'gray.900')}
+        boxShadow={'2xl'}
+        rounded={'md'}
+        p={6}
+        overflow={'hidden'}>
+        <Box
+          h={'210px'}
+          bg={'gray.100'}
+          mt={-6}
+          mx={-6}
+          mb={6}
+          pos={'relative'}>
+        </Box>
+        <Stack>
+          <Text
+            color={'green.500'}
+            textTransform={'uppercase'}
+            fontWeight={800}
+            fontSize={'sm'}
+            letterSpacing={1.1}>
+            Blog
+          </Text>
+          <Heading
+            color={useColorModeValue('gray.700', 'white')}
+            fontSize={'2xl'}
+            fontFamily={'body'}>
+            Boost your conversion rate
+          </Heading>
+          <Text color={'gray.500'}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum.
+          </Text>
+        </Stack>
+        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
+            alt={'Author'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>Achim Rolle</Text>
+            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
+          </Stack>
+        </Stack>
+      </Box>
+    </Center>
+     
     </div>
+    </ChakraProvider>
   );
 }
+
 
 export default App;
