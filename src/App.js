30842805861a6b554a7c18a380cd5573062c264c
Nick Robinson <nickr964@gmail.com>
2023-02-19 16:22:22 -0500

    Adding all as is to use local data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import {   Box,
   Text,
   Stack,
   Avatar,
-  useColorModeValue } from '@chakra-ui/react'
+  useColorModeValue,
+  Select,
+Image } from '@chakra-ui/react'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import React, { useEffect, useState } from "react";
@@ -15,73 +17,75 @@ function App() {
 
   const [bars, setBars] = useState([]);
   useEffect(() => {
-      fetch('http://localhost:8080/bars/all')
+      // fetch('./src/assets/json/BarResponse.json')
+      fetch('data/BarResponse.json')
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             setBars(data);
         })
         .catch((err) => {
+          console.log('in this error?')
             console.log(err.message);
         });
   }, []);
 
   return (
     <ChakraProvider>
-
       <div className="App">
+
       <Center py={6}>
-      <Box
-        maxW={'445px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.900')}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}>
-        <Box
-          h={'210px'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={'relative'}>
-        </Box>
-        <Stack>
-          <Text
-            color={'green.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}>
-            Blog
-          </Text>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize={'2xl'}
-            fontFamily={'body'}>
-            Boost your conversion rate
-          </Heading>
-          <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
-          </Text>
-        </Stack>
-        <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
-          <Avatar
-            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
-            alt={'Author'}
-          />
-          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-            <Text fontWeight={600}>Achim Rolle</Text>
-            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
+        <Select size='lg' placeholder='Select day'>
+          <option value='monday'>Monday</option>
+          <option value='tuesday'>Tuesday</option>
+          <option value='wednesday'>Wednesday</option>
+          <option value='thursday'>Thursday</option>
+          <option value='friday'>Friday</option>
+        </Select>
+      </Center>
+
+        {bars.map((bar) => (
+         <Center py={6}>
+          <Box
+            maxW={'445px'}
+            w={'full'}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            p={6}
+            overflow={'hidden'}>
+            <Box
+              h={'210px'}
+              bg={'gray.100'}
+              mt={-6}
+              mx={-6}
+              mb={6}
+              pos={'relative'}>
+              <Image
+                src={bar.image} layout={'fill'}
+              />
+            </Box>
+          <Stack>
+            <Text
+              color={'green.500'}
+              textTransform={'uppercase'}
+              fontWeight={800}
+              fontSize={'sm'}
+              letterSpacing={1.1}>
+             Quizzo Bar
+            </Text>
+            <Heading
+              
+              fontSize={'2xl'}
+              fontFamily={'body'}>
+              {bar.name}
+            </Heading>
+            <Text color={'gray.500'}>
+            {bar.dayOfWeek} at {bar.time}
+            </Text>
           </Stack>
-        </Stack>
-      </Box>
-    </Center>
-     
+        </Box>
+        </Center>
+          ))}
     </div>
     </ChakraProvider>
   );
